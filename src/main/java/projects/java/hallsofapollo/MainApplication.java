9aed5b13654924d692b10a19510d6c8413d6ee1f
Chris Domingos <chrisd03@outlook.com>
2023-05-11 20:33:11 -0400

    basic javafx scene setup

--- src/main/java/projects/java/hallsofapollo/MainApplication.java
+++ src/main/java/projects/java/hallsofapollo/MainApplication.java
@@ -11,8 +11,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+        stage.setTitle("Halls of Apollo");
         stage.setScene(scene);
         stage.show();
     }
